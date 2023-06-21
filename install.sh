c88f2a36d4011784b88a02aaedb1332334233bce
Kondemahesh <99584470+Kondemahesh@users.noreply.github.com>
2023-06-21 16:38:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-aws s3 cp s3://java-artifacts-devops4solutions/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
-sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
+aws s3 cp s3://codepipeline-ap-south-1-25308380264/springboot/BuildArtif/ /tmp
+sudo mv /tmp/.war /opt/tomcat/webapps/.war
+sudo systemctl start tomcat
